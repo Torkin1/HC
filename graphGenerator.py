306b3890e95b39434e8d191c1aa14a1f59b90d7f
mihaiJianu <45464636+mihaiJianu@users.noreply.github.com>
2019-01-05 19:53:43 +0100

    Add files via upload

---
+++ graphGenerator.py
@@ -0,0 +1,34 @@
+from Graph.graph.Graph_AdjacencyMatrix import *
+from random import randint
+
+def gGenerator(n, rangeG, Ciclo = True):
+    """
+    @param n: int
+    @param range: int
+    @param Ciclo: Bool
+    @return Graph
+
+    n = numero di nodi
+    rangeG = range di numeri casuali, per creare i nodi
+    Ciclo = Se si desidera o meno un grafo con il ciclo
+    """
+
+    if  Ciclo:
+        g = GraphAdjacencyMatrix()
+        for i in range(0, n):
+            g.addNode(randint(0, rangeG))
+        for j in range(0, n + 1):
+            for k in range(0, n + 1):
+                g.insertEdge(j, k, 1)
+        g.print()
+        return
+    else:
+        g = GraphAdjacencyMatrix()
+        for i in range(0, n):
+            g.addNode(randint(0, rangeG))
+        for j in range(0, n):
+            g.insertEdge(j, j + 1, 1)
+        for k in range(0, n):
+            g.insertEdge(k + 1, k, 1)
+        g.print()
+        return
