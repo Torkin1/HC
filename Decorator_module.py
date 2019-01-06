9efac9fd0ac8388a47e6a640b4ea2548cd30eb10
Torkin1 <daniele.laprova2@gmail.com>
2019-01-06 22:20:43 +0100

    modified:   Decorator_module.py

--- Decorator_module.py
+++ Decorator_module.py
@@ -6,12 +6,12 @@ Decorator
 """
 
 #from HCUF_module import *
-from cProfile import runctx, run
+from cProfile import Profile, run
 import pstats
 import functools
-from Graph.graph.Graph_AdjacencyMatrix import *
+#from Graph.graph.Graph_AdjacencyMatrix import *
 
-globalParam = None # necessaria per il profiler, altrimenti è impossibile passare il grafo come argomento del decorator.
+#globalParam = None # necessaria per il profiler, altrimenti è impossibile passare il grafo come argomento del decorator.
 
 def include_stripped(decorator):
     def wrapping_decorator(func):
@@ -20,26 +20,40 @@ def include_stripped(decorator):
         return wrapped
     return wrapping_decorator
 
-
 @include_stripped
 def profiler(func):
-    @functools.wraps(func)
-    def wrapping_function(args):
-        name = func.__name__
-        global globalParam
-        globalParam = args
-        runctx(f'{name}.stripped(args)', globals(), locals(), 'stats.txt')
-        #run(wrapped_stripped(globalParam), 'stats.txt')
-        with open(f'{name}.txt', 'w') as outPutPath:
-            stats = pstats.Stats('stats.txt', stream = outPutPath).strip_dirs().sort_stats("time")
-            stats.print_stats()
-    return wrapping_function
+    def wrapFunction(*args, **kwargs):
+        funcProfile = Profile()
+        valueReturned = funcProfile.runcall(func, *args, **kwargs)
+        funcProfile.dump_stats("tempStats.txt")
+        #run(f"{func.__name__}(*args, **kwargs)", "tempStats.txt")
+        with open(f"{func.__name__}.txt", 'w') as outPutPath:
+            stats = pstats.Stats('tempStats.txt', stream = outPutPath).strip_dirs().sort_stats("time").print_stats()
+
+        return valueReturned
+    return wrapFunction
+
+#
+#
+#@include_stripped
+#def profiler(func):
+#    @functools.wraps(func)
+#    def wrapping_function(args):
+#        name = func.__name__
+#        global globalParam
+#        globalParam = args
+#        runctx(f'{name}.stripped(args)', globals(), locals(), 'stats.txt')
+#        #run(wrapped_stripped(globalParam), 'stats.txt')
+#        with open(f'{name}.txt', 'w') as outPutPath:
+#            stats = pstats.Stats('stats.txt', stream = outPutPath).strip_dirs().sort_stats("time")
+#            stats.print_stats()
+#    return wrapping_function
 
 
-@profiler
-def waste_some_time(num_times):
-    for _ in range(num_times):
-        sum([i ** 2 for i in range(10000)])
+#@profiler
+#def waste_some_time(num_times):
+#    for _ in range(num_times):
+#        sum([i ** 2 for i in range(10000)])
 
-if __name__ == "__main__":
-    waste_some_time(1000)
+#if __name__ == "__main__":
+#    waste_some_time(1000)
