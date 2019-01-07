d851e49cbc8ac86c5deeb42ca8eaa3d3f64342d5
Torkin1 <daniele.laprova2@gmail.com>
2019-01-07 20:06:18 +0100

    modified:   Decorator_module.py 	new file:   Graph/graph/.Graph.py.swp 	new file:   __pycache__/Decorator_module.cpython-36.pyc 	modified:   __pycache__/HCUF_module.cpython-36.pyc 	new file:   __pycache__/script.cpython-36.pyc 	new file:   log.txt

--- Decorator_module.py
+++ Decorator_module.py
@@ -5,8 +5,9 @@ Python version: 3.x
 Decorator
 """
 
-from cProfile import Profile, run
-import pstats
+#from cProfile import Profile, run
+#import pstats
+from time import time, ctime
 
 def include_stripped(decorator):
     
@@ -19,23 +20,41 @@ def include_stripped(decorator):
 
 @include_stripped
 def profiler(func):
-    
+
     def wrapFunction(*args, **kwargs):
+
+        pathToOutput = "log.txt"
         
-        funcProfile = Profile()
-        pathToOutput = f"{func.__name__}" + f"{len(args[0].adj)}" + ".txt"
+        startTime = time()
+        rValue = func(args[0])
+        elapsedTime = time() - startTime
         
-        valueReturned = funcProfile.runcall(func, *args, **kwargs)
-        funcProfile.dump_stats("tempStats.txt")
-        #run(f"{func.__name__}(*args, **kwargs)", "tempStats.txt")
-        
-        with open(pathToOutput, 'w') as fOutput: 
-            fOutput.write(f"Function called: {func.__name__}\n")
-            fOutput.write("\n")
-            fOutput.write(f"Size of input: {len(args[0].adj)} nodes, {args[0].numEdges()} edges\n")
-            fOutput.write("\n")
-            pstats.Stats('tempStats.txt', stream = fOutput).strip_dirs().sort_stats("time").print_stats()
-        
-        return valueReturned
+        with open(pathToOutput, "a") as fOutput:
+            
+            fOutput.write(f"[{ctime(time())}] name: {func.__name__} ; nodes: {len(args[0].adj)} ; edges: {args[0].numEdges()} ; elapsed: {str(elapsedTime)[:7]}s ; return: {rValue}\n")
+        return rValue
+    
     return wrapFunction
+            
+#@include_stripped
+#def profiler(func):
+#    
+#    def wrapFunction(*args, **kwargs):
+#        
+#        funcProfile = Profile()
+#        pathToOutput = f"{func.__name__}" + f"{len(args[0].adj)}" + ".txt"
+#        
+#        valueReturned = funcProfile.runcall(func, *args, **kwargs)
+#        funcProfile.dump_stats("tempStats.txt")
+#        #run(f"{func.__name__}(*args, **kwargs)", "tempStats.txt")
+#        
+#        with open(pathToOutput, 'w') as fOutput: 
+#            fOutput.write(f"Function called: {func.__name__}\n")
+#            fOutput.write("\n")
+#            fOutput.write(f"Size of input: {len(args[0].adj)} nodes, {args[0].numEdges()} edges\n")
+#            fOutput.write("\n")
+#            pstats.Stats('tempStats.txt', stream = fOutput).strip_dirs().sort_stats("time").print_stats()
+#        
+#        return valueReturned
+#    return wrapFunction
 
