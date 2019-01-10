5faabbc7763dd55221d4dd28e48f5288f18fd87b
LorenzoMei <45484506+LorenzoMei@users.noreply.github.com>
2019-01-10 18:46:43 +0100

    Add files via upload

--- HCDFS_module.py
+++ HCDFS_module.py
@@ -8,6 +8,7 @@ Modulo contenente la definizione di hasCycleDFS e delle sue funzioni ausiliarie.
 
 from Graph.graph.Graph_AdjacencyMatrix import *
 from Graph.graph.Graph import *
+from Decorator_module import *
 
 
 class CustomGAM(GraphAdjacencyMatrix):
@@ -24,7 +25,7 @@ class CustomGAM(GraphAdjacencyMatrix):
         self.adj = G.adj
 
 
-    def dfsDetectedCycle(self, rootId):
+    def dfsDetectedCycle(self, rootId, debug):
         """
         Execute a Depth-First Search (DFS) in the graph starting from the
         specified node and check if a cycle is detected.
@@ -45,24 +46,39 @@ class CustomGAM(GraphAdjacencyMatrix):
         explored = {rootId}  # nodes already explored
 
         while not s.isEmpty():  # while there are nodes to explore ...
+            
             node = s.pop()  # get the node from the stack
-            explored.add(node)  # mark the node as explored
-            # add all adjacent unexplored nodes to the stack
+            explored.add(node)  # mark the node as explored 
 
+            # add all adjacent unexplored nodes to the stack
             for adj_node in self.getAdj(node):
+                countSeen = 0   #contatore delle volte in cui è stato visto adj_node nello stack
 
                 if adj_node not in explored:
                     s.push(adj_node)
 
-                elif len(dfs_nodes) > 0 and adj_node != dfs_nodes[-1]:
+                    for elem in s.s:    #conteggio della presenza del nodo nello stack
+                        if adj_node == elem:
+                            count += 1
+
+                if countSeen > 1:    #Se ho visto almeno 2 volte lo stesso nodo nello stack ho un ciclo 
                     return True
+
+                if debug:
+                    print("-------")
+                    print(f"Current node: {node}")
+                    print(f"Adjacent node: {adj_node}")
+                    print(f"Previous node: {prevNode}")
+                    print(f"Explored nodes: {explored}")
+                    print(f"DFS list: {dfs_nodes}")              
+                    print("-------")
 
             dfs_nodes.append(node)
 
         return False
 
-
-def hasCycleDFS(G):
+@profiler
+def hasCycleDFS(G, debug = False, showProfile = False, timeAccuracy = False):
     """
     @param G: Graph
     @return: boolean
@@ -72,7 +88,7 @@ def hasCycleDFS(G):
     graph = CustomGAM(G)
     nodeList = graph.getNodes()
 
-    if graph.dfsDetectedCycle(nodeList[0].id):
+    if graph.dfsDetectedCycle(nodeList[0].id, debug):   #Facciamo partire la visita DFS dal primo nodo presente in nodeList
         return True
 
     return False
